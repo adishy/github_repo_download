3bbfde8ebe48d745c9b76b53dc118c75e358b317
Aditya Shylesh <aditya.shylesh@verizonmedia.com>
2021-09-18 23:29:52 -0500

    Updating node github_sync to save multiple formats and use synchronous exec to download archive

--- github_sync.js
+++ github_sync.js
@@ -1,18 +1,66 @@
-const GitHub = require('github-api');
+const { execSync } = require("child_process");
 const fs = require('fs');
+const GitHub = require('github-api');
 const path = require('path');
 
 function checkArgs() {
-    let requiredArgs = [ "root-dir", "github-personal-access-token" ];
+    let requiredArgs =
+    [ 
+        { name: "rootDir", show: "root-dir", required: true },
+        { name: "token", show: "github-personal-access-token", required: true },
+        { name: "archiveFormat", show: "archive-format: [zip (default), tar]", required: false }
+    ];
     if ( process.argv.length < 2 + requiredArgs.length  ) {
-        let argsDisplay = requiredArgs.map( arg => `<${arg}>` ).join(" ");
+        let argsDisplay = requiredArgs.map( arg => `${ ( arg.required ) ? "reqd" : "opt"}:<${arg.show}>` ).join(" ");
         console.log(`Usage:\n ${process.argv[0]} ${process.argv[1]} ${argsDisplay}`);
         process.exit(1);
     }
-    return {
-        rootDir: process.argv[2],
-        token: process.argv[3]
+    let args = {};
+    for ( [ index, arg ] of process.argv.entries() ) {
+        if ( index < 2) continue;
+        args[requiredArgs[index - 2].name] = arg;
     }
+    return args;
+}
+
+/**
+ downloadArchiveShell: returns curl command <str> used in exec for retrieving repo zipball
+
+ Required arguments:
+ args: 
+    {
+        owner: <str>, // Name of repo owner
+        repo: <str>, // Name of repo
+        branch: <str>, // Name of branch within repo
+        token: <str>, // Personal access token with scopes to retrieve repo archive
+        destinationDir: <str> // Destination directory to which zipball should be downloaded
+    }
+**/
+function downloadArchive(args, format="tar") {
+    let archiveFormats =
+    {
+        "tar": ".tar.gz",
+        "zip": ".zip"
+    };
+    let fullUrl = `https://api.github.com/repos/${args.owner}/${args.repo}/${format}ball/${args.branch}`;
+    let archivePath = path.join(args.destinationDir, `${args.branch}${archiveFormats[format]}`);
+    if ( fs.existsSync(archivePath) ) fs.unlinkSync(archivePath);
+    let curlArgs = 
+    [
+      "curl",
+      "-H", `"Accept: application/vnd.github.v3+json"`,
+      "-H", `"Authorization: token ${args.token}"`,
+      "-L", fullUrl,
+      ">", archivePath
+    ];
+    function handleDownloadError(error, stdout, stderr) {
+        if ( error ) {
+            console.log(" [FAILED!]");
+            console.error(download.error);
+            process.exit(1);
+        }
+    }
+    let download = execSync(curlArgs.join(" "), handleDownloadError);
 }
 
 async function main() {
@@ -31,19 +79,34 @@ async function main() {
     let rootDir = path.resolve(args.rootDir);
     if ( !fs.existsSync(rootDir) ) fs.mkdirSync(rootDir);
 
-    for ( const repo of repositories ) {
+    for ( const [ index, repo ] of repositories.entries() ) {
         // Create directory for repository in specified root-dir
         let repositoryDir = path.join(rootDir, repo.name);
         if ( !fs.existsSync(repositoryDir) ) fs.mkdirSync(repositoryDir);
         
-        //// Retrieve repository and branches
-        //let repository = client.getRepo(repo.owner.login, repo.name);
-        //let allBranches = await repository.listBranches();
-        //let branches = allBranches.data;
-        //let branchNames = branches.map( branch => branch.name ); 
-        //console.log(`${repo.name}: branchNames`);
+        // Retrieve repository and branches
+        let repository = client.getRepo(repo.owner.login, repo.name);
+        let allBranches = await repository.listBranches();
+        let branches = allBranches.data;
+        let branchNames = branches.map( branch => branch.name ); 
+        process.stdout.write(`(${index + 1}/${repositories.length}): ${repo.name} - ${branchNames}`);
 
-        
+        for ( branch of branchNames ) {
+            let archiveFormat = "zip";
+            if ( "archiveFormat" in args ) archiveFormat = args.archiveFormat;
+            downloadArchive(
+                            {
+                                owner: repo.owner.login,
+                                repo: repo.name,
+                                branch: branch,
+                                token: args.token,
+                                destinationDir: repositoryDir
+                            },
+                            archiveFormat
+                           );            
+            await new Promise(r => setTimeout(r, 1000));
+        }
+        console.log(" [DONE!]");
     }
 }
 
